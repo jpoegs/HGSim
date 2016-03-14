d09f68101a42cf5d26a42002dc0723ddd47d4472
jpoegs <jpoegel@comcast.net>
2016-03-14 18:21:49 -0400

    Overhauled the way the server handles game sessions
    Server now no longer has persistence with game sessions. Also adding a
    chat system.

--- HungerGames.js
+++ HungerGames.js
@@ -1,34 +1,227 @@
 
-var numberOfTributes;
-var sessionID;
+var con;
+var io;
+var sessions = [];
+
+function addSession(maxPlayers) {
+	var sess = new session(maxPlayers);
+	sessions.push(sess);
+	return sess.sessionID;
+}
+
+function addPlayer(socket, socketID, name, sessionID) {
+	for(var i = 0; i < sessions.length; i++) {
+		if(sessions[i].sessionID == sessionID) {
+			var nplayer = new player(socket, socketID, name, sessionID);
+			sessions[i].players.push();
+			sessions[i].currentPlayers++;
+			socket.emit('new-player', nplayer);
+			break;
+		}
+	}
+}
+
+function leaveGameSession(socketID) {
+	for(var i = 0; i < sessions.length; i++) {
+		removePlayer(sessions[i], socketID);
+	}
+}
+
+function removePlayer(sessionID, socketID) {
+	for(var i = 0; i < sessions.length; i++) {
+		if(sessions[i].sessionID == sessionID) {
+			for( var k = 0; k < sessions.players.length; k++ ){
+				if(sessions[i].players[k].socketID == socketID) {
+					sessions[i].players[k].socket.emit('player-left', sessions[i].players[k].socketID);
+					sessions[i].players[k].splice(k, sessions.players.length);
+					console.log("Player left the game...");
+					break;
+				}
+				else {
+					console.log("Player not found...");
+				}
+			}
+			break;
+		}
+	}
+}
+
+function getAllPlayers(sessionID) {
+	for(var i = 0; i < sessions.length; i++) {
+		if(sessions.sessionID == sessionID) {
+			io.to(sessionID).emit('get-all-players', sessions[i].players);
+			break;
+		}
+	}
+}
+
+function session(maxPlayers) {
+	this.maxPlayers = maxPlayers;
+	this.players = [];
+	sessionID = randomGameSession();
+	startGame = true;
+	currentPlayers = 0;
+	day = 0;
+	timeOfDay = "12:00:00";
+}
+
+function isGameJoinable(sessionID) {
+	for(var i = 0; i < sessions.length; i++)
+	{
+		if(sessions[i].sessionID == session) {
+			if(sessions[i].startGame = true) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	return false;
+}
+
+function randomGameSession() {
+	console.log('RandomGameSession');
+	var chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+	var session = "";
+
+	for(var i = 0; i < 8; i++) {
+		session += chars.charAt(Math.random() * chars.length);
+	}
+	for(var i = 0; i < sessions.length; i++)
+	{
+		if(sessions.sessionID == session) {
+			return randomGameSession();
+		}
+	}
+	return session;
+}
+
+function player(socket, socketID, name, sessionID) {
+	this.socket = socket;
+	this.socketID = socketID;
+	this.name = name;
+	this.sessionID = sessionID;
+	this.inventory = [];
+	this.numOfKills = 0;
+	this.namesOfKills = [];
+	this.timeOfDeath = "12:00:00";
+}
+
+function setVars(sql, sockio) {
+	con = sql;
+	io = sockio;
+}
 
 function hungerGame(id, num, name) {
 	numberOfTributes = num;
 	sessionID = id;
-	waitForPeopleToJoin();
-	
-	/*
-	CREATE TABLE 'id' (
-	name varchar,
-	inventory  varchar,
-	numOfKills int,
-	namesOfKills varchar,
-	timeOfDeath varchar,
-	)
-
-	INSERT INTO 'id' (name) VALUES
-		(name);
-	*/
 }
 
-function waitForPeopleToJoin() {
+function iintGame(sessionID) {
+	var startGame = 'UPDATE sessions SET startGame = 0 WHERE sessionsID = ' + con.escape(sessionID);
 	
-	
+	con.query(startGame, function(err, res) {
+		if(err) {
+			console.log(err);
+			throw err;
+		}
+		else {
+			io.to(sessionID).emit('start-game');
+			cornacopia(sessionID);
+		}
+	});
+}
+
+function cornacopia(sessionID) {
+	var corn = 'SELECT * FROM cornacopia';
+	getSessionID( sessionID, function(id) {
+		con.query(corn, function(err, res) {
+			if(err) {
+				console.log(err);
+				throw err;
+			}
+			else {
+				var cEvent = res[Math.random() * res.length];
+				io.to(id).emit('event', cEvent);
+				wait(id);
+			}
+		});
+	});
+}
+
+function wait(sessionID) {
+	getSessionID(sessionID, function(id) {
+		setTimeout(function() {
+			io.to(id).emit('end-event');
+		}, 10000 );
+	});
 	
 }
 
-function test() {
-	var t = Tribute("Jeff");
-	var b = t.getPlayer();
-	document.getElementById("tester").innerHTML = b;
+function getRandomEvent(sessionID) {
+	var eventQuery = 'SELECT * FROM questions';
+	getSessionID(sessionID, function(id) {
+		con.query(eventQuery, function(err, res) {
+			if(err) {
+				console.log(err);
+				throw err;
+			}
+			else {
+				var rEvent = res[Math.random() * res.length];
+				io.to(id).emit('event', rEvent);
+				wait(id)
+			}
+		});
+	});
+}
+
+function getRandomDisaster(sessionID) {
+	var disasterQuery = 'SELECT * FROM disastors';
+	getSessionID(sessionID, function(id) {
+		con.query(disasterQuery, function(err, res) {
+			if(err) {
+				console.log(err);
+				throw err;
+			}
+			else {
+				var dEvent = Math.random() * res.length;
+				io.to(id).emit('event', dEvent);
+				wait(id);
+			}
+		});
+	});
+}
+
+function getSessions(socketID, callback) {
+	var query = 'SELECT * FROM sessions';
+	
+	con.query(query, function(err, res) {
+		if(err) {
+			console.log(err);
+			throw err;
+		}
+		else {
+			callback(res);
+		}
+	});
+}
+
+function getSessions(socketID) {
+	getSessions(socketID, function(results) {
+		
+		for(var i = 0; results.length; i++) {
+			getSession(results[i].sessionsID);
+		}
+		
+		function getSession(sess) {
+			var deathTime = new Date();
+			var deathQuery = 'UPDATE ' + con.escape(sess) + ' SET timeOfDeath ' + deathTime + ' WHERE socketID = ' + socketID;
+			con.query()
+		}
+	});
+}
+
+function getSessionID(sessionID, callback) {
+	callback(sessionID);
 }
--- server.js
+++ server.js
@@ -1,30 +1,17 @@
 var express = require('express');
 var util = require('util');
 var session = require('express-session');
-//var http = require('http')
-//var mysql = require('sql')
 var app = express();
-//var server = http.createServer(app)
 var connect = require('connect');
-//var cookieParser = require('cookie-parser')
-//var MemoryStore = connect.middleware.session.MemoryStore
-var game = require('./HungerGames');
+//var game = require('./HungerGames');
 
 // app.set('view engine', 'jade');
 // app.set('view options', {layout: false});
 
 app.use(express.static(__dirname + '/public'));
 
-//app.use(cookieParser());
-//app.use(session({
-//  secret: 'secret'
-//, key: 'express.sid'
-//, store: store = new MemoryStore()
-//}));
-
 app.get('/', function (req, res) {
-  //res.render('index', {value: req.session.value});
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 var server = app.listen(8080, function() {
@@ -38,350 +25,302 @@ var io = require('socket.io').listen(server);
 var mysql = require("mysql");
 
 // Creates a connection to the database
+ /*
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "WmVagQX%#p87",
   multipleStatements: true
-});
+}); */
 
-con.connect(function(err) {
+var pool = mysql.createPool({
+	connectionLimit: 1000,
+	host: "localhost",
+	user: "root",
+	password: "WmVagQX%#p87",
+	multipleStatements: true
+});
+/*
+con.getConnection(function(err, con) {
   if(err) {
     console.log('Error connecting to database');
     return;
   }
   console.log('Connection to the database successfully established');
-});
+});*/
 
 //con.query('CREATE DATABASE HungerGames');
-con.query('USE HungerGames');
+pool.getConnection(function(err, con) {
+	con.query("USE HungerGames", function(err, res){
+		con.release();
+	});
+});
 
-/*
-con.query('ALTER TABLE sessions ADD (\
-	startGame tinyint NOT NUll, \
-	maxPlayers int NOT NULL, \
-	currentPlayers int NOT NULL, \
-	biome varchar(100), \
-	day varchar(100),\
-	PRIMARY KEY(sessionsID))',
-	function(err, result) {
-		if(err) {
-			console.log(err);
+var sessions = [];
+
+var Session = function(maxPlayers) {
+	this.maxPlayers = maxPlayers;
+	this.players = [];
+	this.sessionID = randomGameSession();
+	this.startGame = true;
+	this.currentPlayers = 0;
+	this.day = 0;
+	this.timeOfDay = "12:00:00";
+	this.messageLog = "Welcome to the chat! \n";
+}
+
+var Player = function(socket, socketID, name, sessionID) {
+	this.socket = socket;
+	this.socketID = socketID;
+	this.name = name;
+	this.sessionID = sessionID;
+	this.inventory = [];
+	this.numOfKills = 0;
+	this.namesOfKills = [];
+	this.timeOfDeath = "12:00:00";
+};
+
+function addSession(maxPlayers) {
+	var session = new Session(maxPlayers);
+	sessions.push(session);
+	console.log("Added game to array...");
+	return session;
+}
+
+Session.prototype.addPlayer = function(socket, socketID, name) {
+	var nplayer = new Player(socket, socketID, name);
+	this.players.push(nplayer);
+	this.currentPlayers++;
+	console.log("Added player named: " + name + " with socketID: " + socketID);
+	return nplayer;
+};
+
+Session.prototype.leaveGameSession = function(socketID) {
+	this.removePlayer(socketID);
+};
+
+Session.prototype.removePlayer = function(socketID) {
+	for( var k = 0; k < this.players.length; k++ ){
+		if(this.players[k].socketID == socketID) {
+			var rplayer = this.players[k];
+			this.players.splice(k, 1);
+			console.log("Player left the game...");
+			if(this.players.length == 0) {
+				
+			}
+			return rplayer;
 		}
 		else {
-			console.log('Sessions table successfully created');
+			console.log("Player not found...");
 		}
-	}); */
+	}
+};
 
+Session.prototype.getNames = function() {
+	var names = [];
+	for(var i = 0; i < this.players.length; i++) {
+		names.push(this.players[i].name);
+	}
+	return names;
+};
 
+Session.prototype.getSocketIDs = function() {
+	var socketIDs = [];
+	for(var i = 0; i < this.players.length; i++) {
+		socketIDs.push(this.players[i].socketID);
+	}
+	return socketIDs;
+};
 
-//con.query('CREATE TABLE questions (question varchar(1000) NOT NULL) ' );
-
-/*
-//var sessDB = require('./SessionDataBase');
-con.end(function(err) {
-  // The connection is terminated gracefully
-  // Ensures all previously enqueued queries are still
-  // before sending a COM_QUIT packet to the MySQL server.
-});
-*/
-function getData(data, callback) {
-	callback(data);
-}
-
-function createGameSession(d) {
-	getData(d, function(data) {
-		gameSessionExists(data.sessionID, function(err1, res1) {
-			if(err1) {
-				console.log(err1);
-				throw err1;
-			}
-			else {
-				if( res1.length > 0 ) {
-					console.log('Game session already exists');
-					data.sessionID = randomGameSession();
-					//console.log(res1);
-					createGameSession(data);
-				}
-				else {
-					var sessionData = {sessionsID: data.sessionID, startGame: 1, maxPlayers: data.playerCount, currentPlayers: 1};
-					var playerTable = 'CREATE TABLE ' + data.sessionID + ' (name varchar(100) NOT NULL, socketID varchar(100) NOT NULL, inventory varchar(1000), numOfKills int, namesOfKills varchar(1000), timeOfDeath varchar(100))';
-					var playerData = {name: data.playerName, socketID: data.socket.id};
-
-					con.query('INSERT INTO sessions SET ?', sessionData, function(err2, res2) {
-						if(err2) {
-							console.log(err2);
-							throw err2;
-						}
-						else {
-							console.log('Inserted ' + data.sessionID + ' into session table.');
-							con.query(playerTable, function(err3, res3) {
-								if(err3) {
-									console.log(err3);
-									throw err3;
-								}
-								else {
-									console.log('Created player table for ' + data.sessionID + ' session');
-									con.query('INSERT INTO ' + data.sessionID + ' SET ?', playerData, function(err4, res4) {
-										if(err4) {
-											console.log("0");
-											console.log(err4);
-											throw err4;
-										}
-										else {
-											console.log('Inserted ' + data.playerName + ' into ' + data.sessionID + ' player table.');
-											var id = data.sessionID;
-											var num = data.playerCount;
-											var name = data.playerName;
-											data.socket.join(id);
-											io.to(id).emit('connection-response', 1);
-											io.to(id).emit('store-session', data.sessionID, data.socket.id);
-											io.to(id).emit('new-player', data.playerName, data.socket.id);
-											console.log('Random game session successfully created');
-										}
-									});
-								}
-							});
-						}
-					});
-				}
-			}
-		});
-	});
-}
+Session.prototype.isGameJoinable = function() {
+	if(this.startGame == true) {
+		return true;
+	}
+	return false;
+};
 
 function randomGameSession() {
 	console.log('RandomGameSession');
 	var chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
-	var session = "";
-	
+	var id = "";
+
 	for(var i = 0; i < 8; i++) {
-		session += chars.charAt(Math.random() * chars.length);
+		id += chars.charAt(Math.random() * chars.length);
 	}
-	
-	//console.log('New randomGameSession: ' + session);
-	
-	//d = new Date();
-	//d.setTime(d.getDay() + 1);
-	//data.setItem("sessionID", session);
-	//data.setitem("date", d);
-	//document.redirect("PlayGame.html");
-	//document.getElementById("result").innerHTML = session;
-	return session;
-}
-
-
-function gameSessionExists(sess, callback) {
-	//console.log('Ran validation for: ' + sess);
-	
-	if( sess == "00000000" ) {
-		callback();
+	for(var i = 0; i < sessions.length; i++) {
+		if(sessions.sessionID == id) {
+			return randomGameSession();
+		}
 	}
-	
-	var sessSQL = 'SELECT * FROM sessions WHERE sessionsID = ' + con.escape(sess);
-	con.query( sessSQL,
-	function(err, res) {
-		//console.log(res);
-		callback(err, res);
-	});
+	return id;
 }
 
-function isGameStarting(sess, callback) {
-	
-	var sessSQL = 'SELECT * FROM sessions WHERE sessionsID = ' + con.escape(sess) + ' AND startGame = 1 AND currentPlayers < maxPlayers';
-	con.query( sessSQL, 
-	function(err, res) {
-		callback(err, res);
-	});
-}
-
-function getPlayers(sess, callback) {
-	var sessSQL = 'SELECT * FROM ' + sess;
-	con.query( sessSQL, 
-	function(err, res) {
-		console.log('Getting players for ' + sess);
-		callback(err, res);
-	});
-}
-
-function getPlayerData(res, sock, callback) {
-	callback(res, sock);
-}
-
-function findPlayer(sess, socket) {
-	getPlayerData(sess, socket, function(sess, sock) {
-		var delName = 'DELETE FROM ' + sess + ' WHERE socketID = ' + con.escape(sock);
-		con.query(delName, function(err2, res2) {
-			if(err2) {
-				console.log('1');
-				console.log(err2);
-				throw err2;
-			}
-			else {
-				if(res2.affectedRows > 0) {
-					var sessSQL = 'UPDATE sessions SET currentPlayers = currentPlayers - 1 WHERE sessionsID = ' + con.escape(sess);
-					console.log('3');
-					con.query(sessSQL, function(err3, res3) {
-						if(err3) {
-							console.log(err3);
-							throw err3;
-						}
-						else {
-							console.log('Player left from ' + sess);
-							io.to(sess).emit('player-left', sock);
-						}
-					});
-				}
-			}
-		});
-	});
-}
-
-function leaveGameSession(s) {
-	getData(s, function(sock) {
-		var search = 'SELECT * FROM sessions';
-		con.query(search, function(err, res) {
-			if(err) {
-				console.log(err);
-				throw err;
-			}
-			else {
-				for(var i = 1; i < res.length; i++) {
-					findPlayer(res[i].sessionsID, sock);
-				}
-			}
-		});
-	});
-}
-
-function joinGameSession(data) {
-	var sessSQL = 'UPDATE sessions SET currentPlayers = currentPlayers + 1 WHERE sessionsID = ' + con.escape(data.sessionID) + ' AND startGame = 1 AND currentPlayers < maxPlayers';
-	con.query(sessSQL, function(err, res) {
-		if(err) {
-			console.log(err);
+function getSession(sessionID) {
+	for( var i = 0; i < sessions.length; i++ ) {
+		if(sessions[i].sessionID == sessionID) {
+			return sessions[i];
 		}
-		else {
-			var addName = {name: data.playerName, socketID: data.socket.id};
-			con.query('INSERT INTO ' + data.sessionID + ' SET ?', addName, function(err2, res2) {
-				if(err2) {
-					console.log(err2);
-				}
-				else {
-					getPlayers(data.sessionID, function(err3, res3) {
-						if(err) {
-							console.log(err3);
-							throw err3;
-						}
-						else {
-							var playerNames = [];
-							var playerSockets = [];
-							
-							for(var i = 0; i < res3.length; i++) {
-								playerNames.push(res3[i].name);
-								playerSockets.push(res3[i].socketID);
-							}
-							
-							console.log('The players in ' + data.sessionID + ' are ' + playerNames);
-							io.to(data.sessionID).emit('store-session', data.sessionID, data.socket.id)
-							io.to(data.sessionID).emit('get-all-players', playerNames, playerSockets);;
-							io.to(data.sessionID).emit('new-player', data.playerName, data.socket.id);
-							console.log(data.playerName + ' has joined ' + data.sessionID);
-						}
-					});
-				}
-			});
-		}
-	});
+	}
+	return false;
 }
+
+function getSessionBySocketID(socketID) {
+	for( var i = 0; i < sessions.length; i++ ) {
+		for( var k = 0; k < sessions[i].players.length; k++ ) {
+			if(sessions[i].players[k].socketID == socketID) {
+				return sessions[i];
+			}
+		}
+	}
+	console.log("Unable to find session with socketID: " + socketID);
+	return false;
+}
+
+function removeSession(sessionID) {
+	for(var i = 0; i < sessions.length; i+=) {
+		if(session[i].sessionID == sessionID) {
+			sessions.splice(i, 1);
+			console.log("Successfully removed session with sessionID: " + sessionID);
+			return true;
+		}
+	}
+	console.log("Could not find session to remove with sessionID: " + sessionID);
+	return false;
+}
+
+function startGame(sessionID) {
+	var session = getSession(sessionID);
+	session.startGame = false;
+	io.to(sessionID).emit('initialize-game');
+	io.to(sessionID).emit('question', "This is a question", "Answer 1", "Answer 2", "Answer 3", "Answer 4");
+	timer(sessionID, 15);
+}
+
+function timer(sessionID, seconds) {
+	var timeLeft = seconds;
+	io.to(sessionID).emit('timer', timeLeft);
+	
+	var interval = setInterval(function() {
+		io.to(sessionID).emit('timer', timeLeft);
+		timeLeft--;
+	}, 1000);
+	
+	setTimeout(function() {
+		clearInterval(interval);
+		getNextQuestion(sessionID)
+	}, seconds * 1000);
+}
+
+function getNextQuestion(sessionID) {
+	io.to(sessionID).emit('question', "This is a question", "Answer 1", "Answer 2", "Answer 3", "Answer 4");
+	timer(sessionID, 15);
+}
+
+var items = 
+
+
+//io.set('heartbeat interval', 8);
+//io.set('heartbeat timeout', 15);
 
 io.on('connection', function(socket) {
 
-	socket.on('create-game', function(d) {
-		d.socket = socket;
-		createGameSession(d);
-		//game.hungerGame(id, num, name);
+	console.log('Connection established');
+
+	socket.on('create-game', function(data) {
+		console.log("Attempting to create game..." + data);
+		var session = addSession(data.playerCount);
+		socket.join(session.sessionID);
+		var nplayer = session.addPlayer(socket, socket.id, data.playerName);
+		var name = nplayer.name;
+		var socketID = nplayer.socketID;
+		var sessionID = session.sessionID;
+		socket.emit('connection-response', 1);
+		socket.emit('new-player', name, socketID);
+		socket.emit('store-session', sessionID, socketID);
+		console.log("Finished creating a new game session with id: " + sessionID);
+		socket.emit('server-message', session.messageLog);
 	});
 	
-	socket.on('isValidGameSession', function(sess) {
-		isGameStarting(sess, function(err, res) {
-			console.log('Starting...');
-			console.log(res);
-			if(err) {
-				console.log(err);
-				throw err;
-			}
-			else {
-				if( res.length > 0 ) {	
-					console.log('You can join that game!');
-					socket.emit('getValidation', true)
-				}
-				else {
-					console.log('Cant join that game');
-				}
-			}
-		});
+	socket.on('send-message', function(id, message) {
+		io.to(id).emit('recieve-message', message);
 	});
 	
-	console.log('Player connnected successfully');
+	socket.on('isValidGameSession', function(sessionID) {
+		var session = getSession(sessionID);
+		var join = session.isGameJoinable();
+		console.log("This game is " + join);
+		socket.emit('get-validation', join);
+	});
 	
 	//io.emit("User connected", players)
 	
 	socket.on('join-game', function(data) {
-		data.socket = socket;
+		console.log("SessionID joining... " + data.sessionID);
+		var session = getSession(data.sessionID);
 		socket.join(data.sessionID);
-		joinGameSession(data);
-		//data.sessionID = session;
-		//data.playerName = name;
-		
+		var nplayer = session.addPlayer(socket, socket.id, data.playerName);
+		var names = session.getNames();
+		var socketIDs = session.getSocketIDs();
+		var name = nplayer.name;
+		var socketID = nplayer.socketID;
+		socket.emit('connection-response', 1);
+		socket.emit('get-all-players', names, socketIDs);
+		console.log("The players in this game session are: " + names);
+		io.to(data.sessionID).emit('new-player', name, socketID);
+		socket.emit('store-session', data.sessionID, socketID);
+		socket.emit('server-message', session.messageLog);
+		socket.emit('server-message', name + " joined the game!");
 	});
 	
-	socket.on('get-players', function(sess) {
-		getPlayers(sess, function(err, res) {
-			if(err) {
-				console.log(err);
-				throw err;
-			}
-			else {
-				console.log(res);
-				io.to(ses).emit('get-all-players', res);
-			}
-		});
+	socket.on('get-players', function(sessionID) {
+		var session = getSession(sessionID);
+		var names = session.getNames();
+		var socketIDs = session.getSocketIDs();
+		io.to(sessionID).emit('get-all-players', names, socketIDs);
 	});
 	
+	socket.on('start-game', function(data) {
+		if(data.host == "true") {
+			startGame(data.sessionID);
+		}
+	});
+
+	socket.on('send-message', function(message, sess, name) {
+		var session = getSession(sess);
+		session.messageLog += name + ": " + message + "\n";
+		console.log("MESSAGE SENT!!");
+		io.to(sess).emit('receive-message', name, message);
+		console.log("Message from " + name + " on " + sess + ": " + message);
+	});
+
+	socket.on('remove-player', function(sessionID, socketID) {
+		if(typeof socketID == "undefined" || socketID == "" || socketID == null) {
+			console.log("No player data to remove...");
+			return;
+		}
+		var session = getSession(sessionID);
+		if(session != false) {
+			var rplayer = session.removePlayer(socketID);
+			var name = rplayer.name;
+			io.to(sessionID).emit('player-left', socketID);
+			socket.emit('server-message', name + " joined the game!");
+			console.log('Player left. Removing player...');
+		}
+		else {
+			console.log('Error in removing player by sessionID and socketID...');
+		}
+	});
 	socket.on("disconnect", function() {
-		leaveGameSession(socket.id);
-		console.log('User disconnected');
+		console.log("Trying to remove: " + socket.id);
+		var session = getSessionBySocketID(socket.id);
+		if(session != false) {
+			session.leaveGameSession(socket.id);
+			console.log('Player left. Removing player...');
+		}
+		else {
+			console.log('Error in removing player by socketID...');
+		}
+		console.log('Connection terminated');
 	});
 	
 });
-
-
-
-/*
-io.listen(app).set('authorization', function (data, accept) {
-  if (!data.headers.cookie) 
-    return accept('No cookie transmitted.', false);
-
-  data.cookie = cookieParser(data.headers.cookie);
-  data.sessionID = data.cookie['express.sid'];
-
-  store.load(data.sessionID, function (err, session) {
-    if (err || !session) return accept('Error', false);
-
-    data.session = session;
-    return accept(null, true);
-  });
-}).sockets.on('connection', function (socket) {
-  var sess = socket.handshake.session;
-  socket.log.info(
-      'a socket with sessionID'
-    , socket.handshake.sessionID
-    , 'connected'
-  );
-  socket.on('set value', function (val) {
-    sess.reload(function () {
-      sess.value = val;
-      sess.touch().save();
-    });
-  });
-}); */
